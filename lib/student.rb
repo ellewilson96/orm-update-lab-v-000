29a70d61955e12f47dc0f40748009ef66676b284
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-17 15:56:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,9 @@ class Student
       result = DB[:conn].execute(sql, name)[0]
       Student.new(result[0], result[1], result[2])
     end
-    
+
     def self.new_from_db(row)
-    new_student = self.new(row)
+    new_student = self.new
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
