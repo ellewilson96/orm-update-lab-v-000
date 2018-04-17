33d0170357371191f6248bdbcab338884cb24245
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-17 15:51:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,11 +34,10 @@ class Student
     end
 
     def self.new_from_db(row)
-    new_student = self
-    new_student.id = row[0]
-    new_student.name =  row[1]
-    new_student.grade = row[2]
-    new_student
+    self.id = row[0]
+    self.name =  row[1]
+    self.grade = row[2]
+    self
     end
 
   def save
