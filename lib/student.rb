40b3e9410219c9250d75e6b13163b9c2bf80b283
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-17 15:49:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,44 @@ class Student
       Student.new(result[0], result[1], result[2])
     end
 
+    def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+    end
+
+
+  def save
+      if self.id
+    self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  end
+
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS students"
+      DB[:conn].execute(sql)
+    end
 
     def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
