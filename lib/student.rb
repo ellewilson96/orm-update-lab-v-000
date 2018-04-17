9947232087f8e9dddcb2684ac87419c69be5db3b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-17 15:37:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,29 +46,6 @@ class Student
       DB[:conn].execute(sql)
     end
 
-    def self.drop_table
-      sql = "DROP TABLE IF EXISTS students"
-      DB[:conn].execute(sql)
-    end
-
-    def self.count_all_students_in_grade_9
-      sql = <<-SQL
-      SELECT COUNT(*)
-      FROM students
-      WHERE grade = 9
-      SQL
-      DB[:conn].execute(sql)
-    end
-
-    def self.students_below_12th_grade
-      sql = <<-SQL
-      SELECT COUNT(*)
-      FROM students
-      WHERE grade < 12
-      SQL
-      DB[:conn].execute(sql)
-    end
-
     def self.all
       sql = <<-SQL
       SELECT *
@@ -78,20 +55,5 @@ class Student
       DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
       end
-    end
-
-    def self.first_X_students_in_grade_10(num)
-      sql = "SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT ?;"
-      DB[:conn].execute(sql, num)
-    end
-
-    def self.first_student_in_grade_10
-    student = self.first_X_students_in_grade_10(1).flatten
-    self.new_from_db(student)
-    end
-
-    def self.all_students_in_grade_X(num)
-    sql = "SELECT * FROM students WHERE grade=?;"
-      DB[:conn].execute(sql, num)
   end
-  end
+end
